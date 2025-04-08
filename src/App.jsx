495fb92ec62a8802357eed0dcadfe6c856545ad8
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-08 13:37:17 +0100

    Feat: added help to understand properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  let array = ["year 1", "year 2", "year 3", "year 4", "year 5"]; //to help listing
+  let gender = "girl" //conditional rendering
+  const [x, setx] = useState(0) //for changing state
+
+  const btnClick = () => {
+    console.log("Clicked")
+    setx(x+1);
+    console.log(x);
+  }
   return (
-    <div>App</div>
+    <div>
+ {array.map((year)=> {return <p>{year}</p>})}
+ {gender === "boy" ? <h2>boy</h2>:<h2>girl</h2>}
+
+ {x}
+ <button onClick={(btnClick)}>click me</button>
+    </div>
+   
   )
 }
 
