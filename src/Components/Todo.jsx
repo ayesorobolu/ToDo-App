de923988943d8a49f7d6e8a23f80f4d3cc2b0e24
Ayesoro Boluwatife <abraham@Ayesoros-MacBook-Pro.local>
2025-04-08 15:40:13 +0100

    Feat: added Todo component

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./CSS/Todo.css"
+
+let count = 0
 const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null)
+
+    const addBtn = () => {
+        setTodos([...todos,{no:count++, text:inputRef.current.value, display:""}])
+        inputRef.current.value = "";
+    }
+
+    useEffect(()=>{
+        console.log(todos);
+    },[todos])
+    
   return (
     <div className='todo'>
+    <div className='todo-header'>To-Do List</div>
+
+    <div className='todo-add'>
+       <input ref={inputRef} type="text" placeholder='Add Your Task' className='todo-input' />   
+       <div className='todo-add-btn' onClick={addBtn}>ADD</div>
+    </div>
+
+    <div className='todo-list'></div>
 
     </div>
   )
